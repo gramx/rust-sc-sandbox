3ca1dfe863a1364274b007306dd9d8f9bfc625b0
Grahame <0xgraam@gmail.com>
2022-03-27 09:29:26 -0600

    Refactor tests and address storage

--- src/contract.rs
+++ src/contract.rs
@@ -1,4 +1,4 @@
-use crate::{ error::ContractError, msg::{ExecuteMsg, InstantiateMsg, QueryMsg}, state::ADDRESSES, state::TOTAL,};
+use crate::{ error::ContractError, msg::{ExecuteMsg, InstantiateMsg, QueryMsg}, state::TRANSACTIONS, state::TOTAL,};
 
 #[cfg(not(feature = "library"))]
 
@@ -20,7 +20,8 @@ pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: ExecuteMsg) 
 
 fn add(_deps: DepsMut, _info: MessageInfo, amount: Uint128) -> Result<Response, ContractError> {
     let sender = _info.sender.to_string();
-    ADDRESSES.save(_deps.storage, &sender, &amount)?;
+    //TODO: No test and check for duplicates yet
+    TRANSACTIONS.save(_deps.storage, &sender, &amount)?;
     let running_total = TOTAL.load(_deps.storage)? + &amount;
     let _total = TOTAL.save(_deps.storage, &running_total);
     Ok(Response::new()
@@ -46,75 +47,113 @@ mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
 
+
+    //TODO: Remove totals since that can be caulcated
     #[test]
-    fn test_add_amount_once() {
+    fn test_add_amount_first_person() {
+        //Mock Data Setup
         let mut deps = mock_dependencies(&[]);
         let info = mock_info("sender", &[]);
         let msg = InstantiateMsg {};
+
+        //Start the contract
         let _res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
 
-        let msg = ExecuteMsg::Add {
-            amount: Uint128::new(12),
-        };
-
+        //Setup and add the amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(12)};
         let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        assert_eq!(
-            Response::new()
-                .add_attribute("action","add")
-                .add_attribute("total", "12"),res
-        );
+        //Value returned is as expected
+        assert_eq!(Response::new().add_attribute("action","add").add_attribute("total", "12"),res);
 
-        assert_eq!(
-            Uint128::new(12), TOTAL.load(deps.as_ref().storage).unwrap()
-        );
+        //Value stored is as expected
+        assert_eq!(Uint128::new(12), TOTAL.load(deps.as_ref().storage).unwrap());
+    }
+    
+    #[test]
+    fn test_add_amount_second_person() {
+        //Mock Data Setup
+        let mut deps = mock_dependencies(&[]);
+        let info = mock_info("sender", &[]);
+        let msg = InstantiateMsg {};
+        
+        //Start the contract
+        let _res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
+
+        //Setup and add the amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(12)};
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        //Value returned is as expected
+        assert_eq!(Response::new().add_attribute("action","add").add_attribute("total", "12"),res);
+
+        //Value stored is as expected
+        assert_eq!(Uint128::new(12), TOTAL.load(deps.as_ref().storage).unwrap());
+
+        //Setup and add second amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(32)};
+        let info = mock_info("sender", &[]);
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        
+        //Total value returned is as expected
+        assert_eq!(Response::new().add_attribute("action","add").add_attribute("total", "44"),res);
+        
+        //Total value stored is as expected
+        assert_eq!(Uint128::new(44), TOTAL.load(deps.as_ref().storage).unwrap());
+    }
+
+
+    #[test]
+    fn test_transactions_only_one() {
+        //Mock Data Setup
+        let addr1 = String::from("addr0001");
+        let info = mock_info(addr1.as_ref(), &[]);
+        let mut deps = mock_dependencies(&[]);
+        let msg = InstantiateMsg {};
+
+        //Start the contract
+        let _res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
+
+        //Setup and add the amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(99)};
+        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+        //Total is working correctly
+        assert_eq!(Response::new().add_attribute("action","add").add_attribute("total", "99"),_res);
+        
+        //Stored value lookup by address is working correctly
+        assert_eq!(Uint128::new(99), TRANSACTIONS.load(deps.as_ref().storage, &addr1).unwrap());
     }
 
     
     #[test]
-    fn test_add_amount_twice() {
+    fn test_transactions_more_than_one() {
+        //Mock Data Setup
+        let addr1 = String::from("addr0001");
+        let info = mock_info(addr1.as_ref(), &[]);
         let mut deps = mock_dependencies(&[]);
-        let info = mock_info("sender", &[]);
         let msg = InstantiateMsg {};
+
+        //Start the contract
         let _res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
 
-        let msg = ExecuteMsg::Add {
-            amount: Uint128::new(12),
-        };
+        //Setup and add the amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(10)};
+        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        //Setup and add the second amount
+        let msg = ExecuteMsg::Add {amount: Uint128::new(5)};
+        let addr2 = String::from("addr0002");
+        let info = mock_info(addr2.as_ref(), &[]);
+        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        assert_eq!(
-            Response::new()
-                .add_attribute("action","add")
-                .add_attribute("total", "12"),res
-        );
-
-        assert_eq!(
-            Uint128::new(12), TOTAL.load(deps.as_ref().storage).unwrap()
-        );
-
-        let msg = ExecuteMsg::Add {
-            amount: Uint128::new(32),
-        };
-
-        let info = mock_info("sender", &[]);
-        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        //Total value returned is as expected (this should be in its own test, redundant here)
+        assert_eq!(Response::new().add_attribute("action","add").add_attribute("total", "15"),_res);
         
-        assert_eq!(
-            Response::new()
-                .add_attribute("action","add")
-                .add_attribute("total", "44"),res
-            );
-        
-        
-        assert_eq!(
-            Uint128::new(44), TOTAL.load(deps.as_ref().storage).unwrap()
-        );
-        
+        //First value stored is as expected
+        assert_eq!(Uint128::new(10), TRANSACTIONS.load(deps.as_ref().storage, &addr1).unwrap());
+        //Second value stored is as expected
+        assert_eq!(Uint128::new(5), TRANSACTIONS.load(deps.as_ref().storage, &addr2).unwrap());
     }
-
-    //Needs address tests
-    //ADDRESSES.load(deps.as_ref().storage, "sender").unwrap()
 
 }
--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,16 @@
 use cw_storage_plus::Map;
 use cosmwasm_std::Uint128;
 use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use cosmwasm_std::Addr;
+use serde::{Deserialize, Serialize};
 
-pub const ADDRESSES: Map<&str, Uint128> = Map::new("addresses");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Transaction {
+    pub amount: Uint128,
+    pub sender: Addr,
+}
+
+pub const TRANSACTIONS: Map<&str, Uint128> = Map::new("transaction");
 
 pub const TOTAL: Item<Uint128> = Item::new("total");
